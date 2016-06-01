a834e0ac374e28a9e9c07eceb7eeefbdacb2f687
deji bimbola <dejibimbola@gmail.com>
2016-06-01 23:25:49 +0100

    program utilizing dictionary

---
+++ hashSet Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashSet_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary =new Dictionary<string, int>();
+
+            dictionary.Add("rice", 2);
+            dictionary.Add("beans", 1);
+            dictionary.Add("plantain", 3);
+            dictionary.Add("yam", 4 );
+
+            if (dictionary.ContainsKey("rice"))
+            {
+                int value = dictionary["rice"];
+                Console.WriteLine(value);
+            }
+            Console.ReadLine();
+        }
+    }
+}
